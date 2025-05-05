615c0a5f21394c3375089a425ebaf4c27a052f77
C.H <christopherhernandez52909@gmail.com>
2025-05-05 08:24:39 -0700

    Create Elements.java

---
+++ Elements.java
@@ -0,0 +1,13 @@
+import org.code.theater.*;
+import org.code.media.*;
+
+  /**
+   * Another subclass that uses the BackgroundScene object as a basis.
+   */
+public class Elements extends BackgroundScene {
+
+  public Elements (String fileName, int x, int y){
+    super(fileName, x, y);              
+  }
+  
+}

a593ab99c71219ae8778c03879b5ae6bd085f6b8
C.H <christopherhernandez52909@gmail.com>
2025-05-05 08:24:12 -0700

    Create Character.java

---
+++ Character.java
@@ -0,0 +1,14 @@
+import org.code.theater.*;
+import org.code.media.*;
+
+  /**
+   * Subclass that uses the BackgroundScene object as a basis.
+   */
+public class Character extends BackgroundScene {
+
+  public Character(String fileName, int x, int y){
+    super(fileName, x, y);
+    
+  }
+  
+}
